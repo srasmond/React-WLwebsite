7066c3d27df506ca43c57af8287e466a4f704608
Sammy Rasmond <srasmond@MacBook-Pro-van-Sammy.local>
2018-05-03 02:03:26 +0200

    v1.3.5 - Bug fixing contact form + mail + user messages

--- server/php/mailer.php
+++ server/php/mailer.php
@@ -4,26 +4,29 @@
 // And
 //https://stackoverflow.com/questions/43645142/submit-form-in-react-js-using-ajax
 
-    if ($_SERVER[“REQUEST_METHOD”] == “POST”) {
+    if ($_POST) {
         $name = strip_tags(trim($_POST["userName"]));
         $name = str_replace(array("\r","\n"),array(" "," "),$name);
         // trim() function strips any white space from beginning and end of the string
-        $email = filter_var(trim($_POST["“userEmail”"]), FILTER_SANITIZE_EMAIL);
+        $email = filter_var(trim($_POST["userEmail"]), FILTER_SANITIZE_EMAIL);
         //  strip_tags() function strips all HTML and PHP tags from a variable.
-        $message = strip_tags($_POST["“userMessage”"]);
+        $message = strip_tags($_POST["userMessage"]);
 
         // Check that data was sent to the mailer.
+            //var_dump($email);
+            //var_dump($name);
+            //var_dump($message);
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            echo "Oei! Er was een probleem met uw verzending. Gelieve het formulier opnieuw in te vullen en door te sturen";
             exit;
         }
 
         // Set the recipient email address.
         $recipient = "info@willemloreinklusjesdienst.be";
         // Set the email subject.
-        $subject = "New contact from $name Via React Site";
+        $subject = "Nieuw bericht van $name";
 
         // Build the email content.
         $email_content = "Name: $name\n";
@@ -38,17 +41,17 @@
         if ($success) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Bedankt! Uw bericht werd verzonden en zal spoedig beantwoord worden.";
         } 
         else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn’t send your message.";
+            echo "Oei! Er is iets misgegaan. Wij kunnen momenteel uw bericht niet verzenden.";
             }
     }
     else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Er was een probleem met uw verzending, gelieve opnieuw te proberen.";
     };
 ?>
--- src/scenes/Contact/components/form.jsx
+++ src/scenes/Contact/components/form.jsx
@@ -4,15 +4,17 @@ import $ from 'jquery';
 export class ContactForm extends React.Component {
     constructor() {
       super();
+      this.state = {
+        contactName: "",
+        contactEmail: "",
+        contactMessage: "",
+        type: "",
+        message: ""
+      };
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleNameChange = this.handleNameChange.bind(this);
       this.handleEmailChange = this.handleEmailChange.bind(this);
       this.handleMessageChange = this.handleMessageChange.bind(this);
-      this.state = {
-        contactName: "",
-        contactEmail: "",
-        contactMessage: ""
-      };
     }
     // Change state of input field so text is updated while typing
     handleNameChange(e) {
@@ -30,26 +32,14 @@ export class ContactForm extends React.Component {
         contactMessage: e.target.value,
       });
     }
-    
-
-    handleSubmit(event) {
-/*      
-      if (!event.target.checkValidity()) {
-        this.setState({ displayErrors: true });
-        return;
-      }
-      // form is valid! We can parse and submit data
-      this.setState({ displayErrors: false });
-*/
-      event.preventDefault();
-      //this.setState({
-       // contactEmail: '',
-        //contactMessage: ''
-      //});
-      this.setState({ type: "info", message: "Sending..." });
+    handleSubmit(e) {
+      e.preventDefault();
+      this.setState({
+        type: "info",
+        message: "Sending..."
+      });
       $.ajax({
-        url: process.env.NODE_ENV === "production" ? "https://www.willemloreinklusjesdienst.be/php/mailer.php" :"../../../server/php/mailer.php",
-        //url: "/../mailer.php",
+        url: "https://www.willemloreinklusjesdienst.be/php/mailer.php",
         type: "POST",
         data: {
           userName: this.state.contactName,
@@ -58,47 +48,45 @@ export class ContactForm extends React.Component {
         },
         cache: false,
         success: function(data) {
-          let responseSuccess = data.message;
+          let responseSuccess = data;
           // Success..
-          this.setState({ type: "success", message: responseSuccess });
-
-          //this.setState({ type: "success", message: "We have received your message and will get in touch shortly. Thanks!" });
-          $('#formContact').slideUp();
-          $('#formContact').after(this.state.message);
+          console.log("OK");
+          this.setState({
+            contactName: "success",
+            contactEmail: "success",
+            contactMessage: "OK",
+            type: "success",
+            message: responseSuccess + "<br/><br/><br/>"
+          });
+          // Remove the contact form and show success message
+          $('#contactForm').slideUp();
+          $('#infoText').slideUp();
+          $('#userMessage').html(this.state.message);
+          //$('#contactForm').after(this.state.message);
           console.log('success', data);
         }.bind(this),
         // Fail
         error: function(xhr, status, err) {
+          console.log("NOK");
           console.log(xhr, status);
           console.log(err);
-          let responseFail = err.message;
-          this.setState({ type: "danger", message: responseFail });
-          //this.setState({ type: "danger", message: "Sorry, there has been an error.  Please try again later or visit us at SZB 438." });
-          console.log(this.state.message + 'fail');
+          let responseFail = xhr.responseText;
+          this.setState({
+            type: "danger",
+            message: responseFail + "<br/><br/><br/>"
+          });
+          // Show error message to the user
+          $('#userMessage').html(this.state.message);
+          console.log(this.state.message + ' fail');
         }.bind(this)
       });
-        
-        
-      /* TODO:
-      * Correctly retrieve data
-      * Send to email via API
-      */
-     /*
-      const data = new FormData(event.target);
-      console.log(data.values);
-      fetch('/api/form-submit-url', {
-        method: 'POST',
-        body: data,
-      });
-      */
     }
   
     render() {
-      const displayErrors  = this.state;
+      //const displayErrors  = this.state;
       return (
         <form 
             id="contactForm" 
-            className={displayErrors ? 'displayErrors' : ''}
             method="POST"
             action="mailer.php"
             onSubmit={this.handleSubmit}  
@@ -113,7 +101,6 @@ export class ContactForm extends React.Component {
             onChange={this.handleNameChange}
             required 
           />
-  
           <input 
             type="email" 
             name="userEmail" 
@@ -124,7 +111,6 @@ export class ContactForm extends React.Component {
             onChange={this.handleEmailChange}
             required 
           />
-
           <textarea 
             name="userMessage" 
             id="userMessage"
@@ -136,7 +122,6 @@ export class ContactForm extends React.Component {
             required
           >
           </textarea>
-  
           <button id="form-submit" className="btn">Versturen</button>
         </form>
       );
